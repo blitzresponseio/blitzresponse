7c5190ccc033d78aa43a2d590a2430b359ba522e
blitzresponseio <blitzresponseio@gmai.com>
2026-03-19 22:02:34 -0700

    skip type checks on build

--- next.config.js
+++ next.config.js
@@ -6,5 +6,11 @@ const nextConfig = {
       { protocol: "https", hostname: "*.supabase.co" },
     ],
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 module.exports = nextConfig;
